4b9ca22a39061f44463aa145e3d658ebd4fb2fed
keggre <kegocandy@gmail.com>
2023-01-08 08:34:47 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,1 +1,47 @@
-#TODO
+from distutils.core import setup
+import py2exe
+import os
+import glob
+
+def find_data_files(source,target,patterns):
+    """Locates the specified data-files and returns the matches
+    in a data_files compatible format.
+
+    source is the root of the source data tree.
+        Use '' or '.' for current directory.
+    target is the root of the target data tree.
+        Use '' or '.' for the distribution directory.
+    patterns is a sequence of glob-patterns for the
+        files you want to copy.
+    """
+    if glob.has_magic(source) or glob.has_magic(target):
+        raise ValueError("Magic not allowed in src, target")
+    ret = {}
+    for pattern in patterns:
+        pattern = os.path.join(source,pattern)
+        for filename in glob.glob(pattern):
+            if os.path.isfile(filename):
+                targetpath = os.path.join(target,os.path.relpath(filename,source))
+                path = os.path.dirname(targetpath)
+                ret.setdefault(path,[]).append(filename)
+    return sorted(ret.items())
+
+setup(
+	windows=[{
+		"script": "dmrclient.py",
+		"icon_resources": [(1, "resources/icon.ico")],
+		"name": "DMR Client",
+		"author": "DMR",
+        "description": "DMR Multiplayer Mod Client",
+		"version": "v1.0.0 Alpha"
+	}],
+	options={
+		"py2exe": {
+			"packages": [],
+			"optimize": 2,
+			"compressed": True,
+			"excludes": ["doctest", "pdb", "difflib", "inspect", "_ssl", "tc"] # removed "Tkinter"
+		}
+	},
+	data_files=find_data_files('resources','resources',['*'])
+)
