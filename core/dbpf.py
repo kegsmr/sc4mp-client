b681ca08dd527dae084e1fe05dca0aa73a7209d0
keggre <kegocandy@gmai.com>
2024-12-01 18:23:11 -0500

    Cleaned up some print statements

--- core/dbpf.py
+++ core/dbpf.py
@@ -260,7 +260,7 @@ class DBPF:
 	def get_SC4ReadRegionalCity(self):
 		"""TODO"""
 
-		print(f'Parsing region view subfile of "{self.filename}"...')
+		#print(f'Parsing region view subfile of "{self.filename}"...')
 
 		# Decompress the subfile and get the data as a bytes stream
 		data = self.decompress_subfile("ca027edb")
