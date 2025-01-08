9139a5889ed224c7a8415610a12b55ac6b4f420f
keggre <kegocandy@gmai.com>
2025-01-07 20:30:42 -0500

    Removed canvas borders.

--- sc4mpclient.py
+++ sc4mpclient.py
@@ -1874,7 +1874,7 @@ class ServerList(th.Thread):
 								except Exception:
 									rank = 0
 								x, y, w, h = self.ui.tree.bbox(server_id, column="#5")
-								canvas = tk.Canvas(width=w, height=h, bd=0)
+								canvas = tk.Canvas(width=w, height=h, bd=0, bg="white", highlightthickness=0, relief='flat')
 								canvas.image = self.rank_bar_images[round(rank)]
 								canvas.create_image(w / 2 + 2, h / 2, anchor="center", image=canvas.image)
 								canvas.place(x=15+x, y=155+y)	
