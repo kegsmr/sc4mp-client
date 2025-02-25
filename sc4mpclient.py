9fa8290cb6ec6b1d8d213d93ecb456c1dc825ba6
keggre <kegocandy@gmai.com>
2025-02-24 23:39:52 -0500

    Added SC4 intro.

--- sc4mpclient.py
+++ sc4mpclient.py
@@ -3173,6 +3173,7 @@ class ServerLoader(th.Thread):
 			"sc4-thumbnail-fix.dll", 
 			"sc4-thumbnail-fix-license.txt", 
 			"sc4-thumbnail-fix-third-party-notices.txt",
+			"sc4mp-intro.dat",
 			"sc4mp-local.dat",
 			"sc4mp-ui.dat",
 		]:
