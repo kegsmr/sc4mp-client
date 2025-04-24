7479644849195c007f9d072f88f57e52039ac663
keggre <kegocandy@gmai.com>
2025-04-24 16:50:25 -0400

    Groundwork for new networking implementation.

--- core/networking.py
+++ core/networking.py
@@ -64,13 +64,6 @@ def recv_json(s, length_encoding="I"):
 class Socket(socket.socket):
 
 
-	def __init__(self, timeout=10):
-
-		super().__init__()
-
-		self.settimeout(timeout)
-
-
 	def send_json(self, data, **kwargs):
 		
 		return send_json(self, data, **kwargs)
@@ -81,9 +74,136 @@ class Socket(socket.socket):
 		return recv_json(self, **kwargs)
 	
 
-	def request(self, request, **kwargs):
+	def recv_exact(self, length):
 
-		data = kwargs
-		data["request"] = request
+		data = b""
 
-		self.send_json(data)
+		while len(data) < length:
+			data += self.recv(length - len(data))
+
+		return data
+	
+
+	def send_message(self, is_request=True, command="Ping", headers=None):
+
+		if headers is None:
+			headers = {}
+
+		m = "SC4MP"
+		if is_request:
+			m += "Req"
+		else:
+			m += "Res"
+		m += command
+
+		message = m.encode('ascii')
+
+		while len(message) < 14:
+			message += b"\x00"
+
+		h = json.dumps(headers).encode()
+		l = struct.pack("H", len(h))
+
+		message += l + h
+
+		print(message) #TODO remove
+
+		self.send(message)
+
+
+	def recv_message(self):
+
+		print("RECEIVING MESSAGE") #TODO
+
+		p = self.recv_exact(5).decode('ascii')
+		if p != "SC4MP": return
+
+		print("PROTOCOL CHECKED")
+
+		t = self.recv_exact(3).decode('ascii')
+		if t == "Req":
+			is_request = True
+		elif t == "Res":
+			is_request = False
+		else: return
+
+		c = self.recv_exact(6)
+		command = c.rstrip(b"\x00").decode('ascii')
+
+		l = struct.unpack("H", self.recv_exact(2))[0]
+		headers = json.loads(self.recv_exact(l).decode())
+
+		return is_request, command, headers
+		
+
+	def request(self, command, headers=None):
+
+		self.send_message(True, command, headers)
+		
+		is_request, c, h = self.recv_message()
+
+		if is_request is False and c == command:
+			return h
+
+
+class ClientSocket(Socket):
+
+	
+	def __init__(self, address, timeout=10, **kwargs):
+
+		super().__init__(**kwargs)
+
+		self.settimeout(timeout)
+
+		self.connect(address)
+
+	
+	def ping(self, headers=None):
+
+		return self.request("Ping", headers)
+
+
+class ServerSocket(Socket):
+
+
+	def __init__(self, address):
+
+		super().__init__()
+
+		self.bind(address)
+	
+	def listen(self, backlog=5):
+		return super().listen(backlog)
+
+	def accept(self):
+		conn, addr = super().accept()
+		return Socket(_sock=conn), addr
+
+	def respond(self, command, headers=None):
+		return self.send_message(False, command, headers)
+
+
+if __name__ == "__main__":
+
+	import threading as th
+
+	address = ("127.0.0.1", 7239)
+
+	def test_serve():
+		s = ServerSocket(address)
+		s.listen()
+		while True:
+			c, _ = s.accept()
+			_, command, headers = c.recv_message()
+			print("MESSAGE RECEIVED")
+			c.respond(command, headers)
+
+	th.Thread(target=test_serve).start()
+
+	time.sleep(5)
+
+	s = ClientSocket(address)
+
+	print(s.ping({
+		"test":"test"
+	}))
